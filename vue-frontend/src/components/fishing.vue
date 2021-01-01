9658aea093ec052fb5cdd4c7b32203d278118813
Kim-SiHwan <tjdans4166@naver.com>
2021-01-01 20:44:31 +0900

    mainApp

---
+++ vue-frontend/src/components/fishing.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-container>
+
+    </v-container>
+  </v-app>
+
+</template>
+
+<script>
+export default {
+  name: "fishing"
+}
+</script>
+
+<style scoped>
+
+</style>
