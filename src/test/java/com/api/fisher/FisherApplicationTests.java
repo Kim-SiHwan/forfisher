2571fb594ffd60c61bf89749c52a664423a36050
Kim-SiHwan <tjdans4166@naver.com>
2021-01-01 18:44:15 +0900

    start

---
+++ src/test/java/com/api/fisher/FisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
